0997386f4e10f658fbf504db3358a7231d8af5ef
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2022-09-17 21:56:18 -0300

    att

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,19 @@
+function tabuada () {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = '' // para limpar o quadro da tabuada antes de aparecer outra se não vai aparecer todas uma em baixo da outra
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}` // para que você consiga selecionar uma linha de conta da tabuada
+            tab.appendChild(item) // para aparecer a conta, se não, não aparece
+            c++
+        }
+    }
+
+}
